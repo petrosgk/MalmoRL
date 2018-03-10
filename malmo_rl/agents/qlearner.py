0514e20bde49e32e5032b220918edd3929af9abd
Peter Giannakopoulos <petrosgk@di.uoa.gr>
2018-03-10 03:19:18 +0200

    Fixed training multiple agents with tensorflow backend

--- malmo_rl/agents/qlearner.py
+++ malmo_rl/agents/qlearner.py
@@ -1,8 +1,15 @@
 import os
 import numpy as np
 from agent import BaseAgent
+
+try:
+    import tensorflow as tf
+except ImportError:
+    tf = None
+
+from keras.backend import tensorflow_backend, image_data_format
 from keras.optimizers import Adam
-import keras.backend as K
+
 from rl.callbacks import ModelIntervalCheckpoint
 from rl.core import Processor
 from rl.memory import SequentialMemory
@@ -16,9 +23,9 @@ class QLearner(BaseAgent):
     def __init__(self, name, env, grayscale, width, height):
         super(QLearner, self).__init__(name=name, env=env)
 
-        self.nb_actions = self.env.available_actions
-        self.abs_max_reward = self.env.abs_max_reward
-        self.mission_name = self.env.mission_name
+        self.nb_actions = env.available_actions
+        self.abs_max_reward = env.abs_max_reward
+        self.mission_name = env.mission_name
 
         self.grayscale = grayscale
         self.width = width
@@ -27,6 +34,12 @@ class QLearner(BaseAgent):
         self.recurrent = False  # Use LSTM
         self.batch_size = 32
         self.window_length = 4
+
+        if tf:
+            config = tf.ConfigProto()
+            config.gpu_options.allow_growth = True
+            sess = tf.Session(config=config)
+            tensorflow_backend.set_session(session=sess)
 
         if not self.recurrent:
             self.model = Minecraft(self.window_length, self.grayscale, self.width, self.height, self.nb_actions)
@@ -88,23 +101,23 @@ class MalmoProcessor(Processor):
                 for i in range(self.window_length):
                     states.append(batch[:, i, :, :, :])
                 # Concatenate states in the batch along the channel axis
-                if K.image_data_format() == 'channels_last':
+                if image_data_format() == 'channels_last':
                     processed_batch = np.concatenate(states, axis=3)
                 else:
                     processed_batch = np.concatenate(states, axis=3).transpose((0, 3, 1, 2))  # Channels-first order
             else:
-                if K.image_data_format() == 'channels_last':
+                if image_data_format() == 'channels_last':
                     processed_batch = batch
                 else:
                     processed_batch = batch.transpose((0, 1, 4, 2, 3))  # Channels-first order
         else:
             if not self.recurrent:
-                if K.image_data_format() == 'channels_last':
+                if image_data_format() == 'channels_last':
                     processed_batch = batch.transpose((0, 2, 3, 1))
                 else:
                     processed_batch = batch
             else:
-                if K.image_data_format() == 'channels_last':
+                if image_data_format() == 'channels_last':
                     processed_batch = np.expand_dims(batch, axis=4)
                 else:
                     processed_batch = np.expand_dims(batch, axis=2)
