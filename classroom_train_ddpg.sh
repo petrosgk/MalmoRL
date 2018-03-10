08ff684bb44947ff2c1cd2a96447a13b35372c8b
Peter Giannakopoulos <petrosgk@di.uoa.gr>
2018-03-10 03:22:01 +0200

    Update classroom_train_ddpg.sh

--- classroom_train_ddpg.sh
+++ classroom_train_ddpg.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-THEANO_FLAGS='floatX=float32, device=cuda' python run_classroom.py --ms-per-tick 10 --clients clients.txt --steps 1000000 --action-space continuous --agent ddpg --mode training
+python run_classroom.py --ms-per-tick 10 --clients clients.txt --steps 1000000 --action-space continuous --agent ddpg --mode training
