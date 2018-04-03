97af63d11430e5bce30bb84f79c6edbd075cf2ec
Peter Giannakopoulos <petrosgk@di.uoa.gr>
2018-04-03 12:43:00 +0300

    Update random_agent.py

--- malmo_rl/agents/random_agent.py
+++ malmo_rl/agents/random_agent.py
@@ -8,17 +8,11 @@ class Random(BaseAgent):
 
         self.nb_actions = env.available_actions
         self.agent = RandomAgent(nb_actions=self.nb_actions)
-        self.agent.compile(None, None)
 
     def fit(self, env, nb_steps):
+        # Crashes for verbose=1
         self.agent.fit(env, nb_steps, action_repetition=4)
 
     # Fitting and testing for the random agent are the same.
     def test(self, env, nb_steps):
         self.fit(env, nb_steps)
-
-    def save(self, out_dir):
-        pass
-
-    def load(self, out_dir):
-        pass
